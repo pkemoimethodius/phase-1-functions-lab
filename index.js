a1a19cecbc3a353795c74bacf72704e24b9bc4b1
methodius pkemoi <pkemoimethodius@gmail.com>
2024-06-27 09:14:21 +0300

    Adds the test requirment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const headQuaters=42;
+
+function distanceFromHqInBlocks(numOfBlocks){
+ if (numOfBlocks>headQuaters){
+  return numOfBlocks-headQuaters;
+ }else{
+  return headQuaters-numOfBlocks;
+ }
+}
+function distanceFromHqInFeet(numOfFeet){
+return distanceFromHqInBlocks(numOfFeet)*264;
+}
+function distanceTravelledInFeet(startingBlock,endingBlock){
+/*if (startingBlock<endingBlock){
+  return (endingBlock-startingBlock)*264;
+}else{
+  return (startingBlock-endingBlock)*264;
+}*/
+return Math.abs(startingBlock-endingBlock)*264
+}
+
+function calculatesFarePrice(startingBlock, endingBlock){
+
+  const distance = distanceTravelledInFeet(startingBlock,endingBlock);
+
+  if (distance<=400){
+
+    return 0;
+  }
+  else if(distance>400 && distance <= 2000)
+    {
+      return (distance-400)*0.02;
+    }
+  else if (distance >2000 && distance <= 2500){
+
+    return 25;
+  }else{
+    return 'cannot travel that far'
+  }
+
+
+}
